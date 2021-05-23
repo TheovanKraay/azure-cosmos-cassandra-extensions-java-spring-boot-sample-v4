e7a64ea8d277bcfce9137b63cbd88d3a8cfd1113
David Noble <David.Noble@microsoft.com>
2021-05-23 14:43:21 -0700

    Fixed checkstyle and spotbugs issues after bumping checkstyle version number.

---
+++ src/test/java/com/azure/cosmos/cassandra/example/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Verifies the spring-boot-app example.
+ */
+package com.azure.cosmos.cassandra.example;
